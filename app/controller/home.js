3e240db7385c2f1d2f8eee013dfdf16bef96e34b
Weipeng Zhang <weipeng.zhang@17zuoye.com>
2021-02-12 21:24:28 +0800

    first commit

---
+++ app/controller/home.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+const ccxt = require('ccxt');
+
+class HomeController extends Controller {
+  async index() {
+    const { ctx } = this;
+
+    const huobi = new ccxt.huobipro();
+    ctx.body = await huobi.loadMarkets();
+
+  }
+}
+
+module.exports = HomeController;
