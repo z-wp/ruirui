588ad09b04680a43ad24ac9df7d31b813b41d672
Weipeng Zhang <weipeng.zhang@17zuoye.com>
2021-02-13 16:28:43 +0800

    update

--- app/controller/home.js
+++ app/controller/home.js
@@ -7,8 +7,11 @@ class HomeController extends Controller {
   async index() {
     const { ctx } = this;
 
-    const okex = new ccxt.okex();
-    ctx.body = await okex.loadMarkets();
+    const okex = new ccxt.okex({
+      apiKey: '22ce605a-309b-43c8-a455-f9acde887113',
+      secret: 'E69A9A2EAA14BF5A0DC4C4AE21D87C7C',
+    });
+    ctx.body = await okex.fetchTicker('BTC/USD');
 
   }
 }
