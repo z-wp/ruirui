103c59477cfbebed8cb64582d280570dc75c0ea6
Weipeng Zhang <weipeng.zhang@17zuoye.com>
2021-02-13 16:01:42 +0800

    update

--- app/controller/home.js
+++ app/controller/home.js
@@ -8,7 +8,7 @@ class HomeController extends Controller {
     const { ctx } = this;
 
     const huobi = new ccxt.huobipro();
-    ctx.body = await huobi.fetchMarkets();
+    ctx.body = await huobi.fetchMarket('ETH/USDT');
 
   }
 }
