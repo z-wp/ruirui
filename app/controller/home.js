729fe4c6933938c070566bc7dca9ef44f2492907
Weipeng Zhang <weipeng.zhang@17zuoye.com>
2021-02-13 16:54:24 +0800

    update

--- app/controller/home.js
+++ app/controller/home.js
@@ -11,8 +11,18 @@ class HomeController extends Controller {
       apiKey: '22ce605a-309b-43c8-a455-f9acde887113',
       secret: 'E69A9A2EAA14BF5A0DC4C4AE21D87C7C',
     });
-    ctx.body = await okex.fetchOHLCV('ETH/USDT');
+    const list = await okex.fetchOHLCV('ETH/USDT', '1d');
 
+    const show = list.map(item => {
+      return {
+        time: new Date(item.shift()).toDateString('Y-m-d H:i:s'),
+        open: item.shift(),
+        high: item.shift(),
+        low: item.shift(),
+        close: item.shift(),
+      }
+    });
+    ctx.body = show;
   }
 }
 
