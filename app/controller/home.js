212f605a1e2fe3d54151a0cd8d321fa56547cc2b
Weipeng Zhang <weipeng.zhang@17zuoye.com>
2021-02-21 17:41:45 +0800

    update

--- app/controller/home.js
+++ app/controller/home.js
@@ -15,7 +15,7 @@ class HomeController extends Controller {
 
     const okex = new ccxt.okex(this.config.okex);
     // const list = await okex.fetchOHLCV('ETH/USDT', '1d', undefined, 20);
-    const data = okex.fetchMyTrades('ETH/USDT', undefined, 10);
+    const data = okex.fetchMyTrades('ETH/USDT', undefined, 10, { type: 'spot' });
     ctx.body = data;
 
     // const show = list.map(item => {
