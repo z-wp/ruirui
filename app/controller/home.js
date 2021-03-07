2a04e564ed93741491c6fb7606e1370191b3e245
Weipeng Zhang <weipeng.zhang@17zuoye.com>
2021-03-08 00:45:02 +0800

    update

--- app/controller/home.js
+++ app/controller/home.js
@@ -10,7 +10,7 @@ class HomeController extends Controller {
     const platform = this.ctx.service.apiCcxt.platformBinance(this.config.binance);
     // const symbol = 'ETH/USDT';
     // const timeframe = '1h';
-    const data = await platform.fetchTickersByType('spot', [ 'ETH/USDT', 'BNB/USDT' ]);
+    const data = await platform.fetchTickers([ 'ETH/USDT', 'BNB/USDT' ], { type: 'spot' });
     ctx.body = data;
     // const algo = await this.ctx.service.haigui.algo(platform, symbol, timeframe);
     // ctx.body = { algo };
